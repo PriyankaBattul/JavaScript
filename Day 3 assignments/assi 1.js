9ad2a00c747c7cefddaf4ee1d36e073056127c9f
PriyankaBattul <68224252+PriyankaBattul@users.noreply.github.com>
2020-07-17 13:29:00 +0530

    Add files via upload

---
+++ Day 3 assignments/assi 1.js
@@ -0,0 +1,16 @@
+let number= prompt("Enter your number",0);
+console.log(number);
+function isEven(value) {
+    if (value%2 == 0){
+    console.log('your number is even')
+        return true;
+    }
+    else{
+        console.log('your number is odd')
+    }
+   
+
+
+return false;
+
+}
